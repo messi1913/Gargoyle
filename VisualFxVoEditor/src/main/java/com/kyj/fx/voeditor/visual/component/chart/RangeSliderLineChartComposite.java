6396bc960c843363d6a54e2f62637fb81ad22bdd
KYJ <KYJ@KYJ-PC>
2016-08-15 22:37:42 +0900

    Slider 차트 컴포넌트 기능 추가.

--- VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/chart/RangeSliderLineChartComposite.java
+++ VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/chart/RangeSliderLineChartComposite.java
@@ -9,21 +9,21 @@ package com.kyj.fx.voeditor.visual.component.chart;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.kyj.fx.voeditor.visual.framework.annotation.FXMLController;
+import org.controlsfx.control.RangeSlider;
+
 import com.kyj.fx.voeditor.visual.util.FxCollectors;
-import com.kyj.fx.voeditor.visual.util.FxUtil;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 
 /***************************
@@ -31,47 +31,67 @@ import javafx.scene.layout.BorderPane;
  * @author KYJ
  *
  ***************************/
-@FXMLController(value = "SliderLineChartView.fxml", isSelfController = true)
-public abstract class SliderLineChartComposite<T> extends BorderPane implements ListChangeListener<T> {
 
-	@FXML
-	public LineChart<String, Number> chart;
+public abstract class RangeSliderLineChartComposite<T> extends BorderPane implements ListChangeListener<T> {
 
-	@FXML
-	public Slider slider;
+	public LineChart<String, Number> chart = new LineChart<>(new CategoryAxis(), new NumberAxis());
+
+	public RangeSlider slider = new RangeSlider();
 
 	//전체 데이터.
 	private ObservableList<T> items = FXCollections.observableArrayList();
 
 	private DoubleProperty displaySize = new SimpleDoubleProperty(15d);
 
-	public SliderLineChartComposite() throws Exception {
-		FxUtil.loadRoot(SliderLineChartComposite.class, this);
-
+	public RangeSliderLineChartComposite() throws Exception {
+		this.setCenter(chart);
+		this.setBottom(slider);
+		initialize();
 	}
 
-	@FXML
+	//	@FXML
 	public void initialize() {
+		chart.setAnimated(false);
+
 		items.addListener(this);
-		//		chart.getData().add(chartSeries);
+
+		slider.setMajorTickUnit(5d);
+
+		slider.setMajorTickUnit(5d);
 
 		slider.setMin(0D);
-		slider.setMax(displaySize.get());
+		slider.setMax(0D);
+		slider.setLowValue(0D);
+		slider.setHighValue(0D);
+
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
+		slider.setBlockIncrement(10);
+		
+		
 		this.displaySize.bind(slider.maxProperty());
 
-		slider.valueProperty().addListener((oba, oldval, newval) -> {
-
+		slider.lowValueProperty().addListener((oba, oldval, newval) -> {
 			int start = newval.intValue();
-			int end = (int) (start + displaySize.get());
-			if (end > items.size())
-				end = items.size();
-			ObservableList<Data<String, Number>> subList = items.stream().skip(start).limit(5).map(v -> converter.apply(v))
+			ObservableList<Data<String, Number>> subList = items.stream().skip(start).limit(displaySize.intValue())
+					.map(v -> converter.apply(v)).collect(FxCollectors.toObservableList());
+			Series<String, Number> chartSeries = new Series<>("", subList);
+			chart.getData().clear();
+			chart.getData().add(chartSeries);
+		});
+
+		slider.highValueProperty().addListener((oba, oldval, newval) -> {
+			int start = (int) slider.getLowValue();
+			int size = Math.abs(start - newval.intValue() - 2);
+			ObservableList<Data<String, Number>> subList = items.stream().skip(start).limit(size).map(v -> converter.apply(v))
 					.collect(FxCollectors.toObservableList());
 			Series<String, Number> chartSeries = new Series<>("", subList);
-			chart.getData().remove(0);
+			chart.getData().clear();
 			chart.getData().add(chartSeries);
-
 		});
+
+		
+
 	}
 
 	@Override
@@ -79,17 +99,24 @@ public abstract class SliderLineChartComposite<T> extends BorderPane implements 
 		if (c.next()) {
 
 			if (c.wasAdded()) {
-				int start = (int) slider.getValue();
-				int end = (int) (start + displaySize.get());
+				int start = 0;
+				int end = 5;
 				Series<String, Number> chartSeries = new Series<>();
 				chartSeries.getData().addAll(items.subList(start, end).stream().map(v -> converter.apply(v)).collect(Collectors.toList()));
 				chart.getData().add(chartSeries);
+				
+				this.slider.setMax(this.items.size());
+				this.slider.setLowValue(5D);
+				this.slider.setHighValue(displaySize.doubleValue());
+				
 			}
 
 			else if (c.wasRemoved()) {
 
 			}
-			this.slider.setMax(items.size());
+
+			
+			
 		}
 
 	}
