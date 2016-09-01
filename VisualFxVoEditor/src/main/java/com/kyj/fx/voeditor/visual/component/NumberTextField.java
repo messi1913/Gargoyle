ae49ee91b3d5d33b9e726bfc2e478b5e8631b772
KYJ <KYJ@KYJ-PC>
2016-09-02 00:37:33 +0900

    잘못된 패키지 이동.

--- VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/NumberTextField.java
+++ VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/NumberTextField.java
@@ -4,7 +4,7 @@
  *	작성일   : 2016. 8. 30.
  *	작성자   : KYJ
  *******************************/
-package com.kyj.fx.voeditor.visual;
+package com.kyj.fx.voeditor.visual.component;
 
 import javafx.scene.control.TextField;
 
