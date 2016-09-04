12a647e4a3af5fc690597dcaff2c4e6161320b0b
KYJ <KYJ@KYJ-PC>
2016-09-04 22:49:07 +0900

    insert script.

--- VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/macro/FacvorScript.sql
+++ VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/macro/FacvorScript.sql
@@ -1,3 +1,5 @@
+##mariadb
+
 CREATE TABLE `tbp_sys_dao_columns` (
 	`PACKAGE_NAME` VARCHAR(200) NOT NULL,
 	`CLASS_NAME` VARCHAR(50) NOT NULL,
@@ -11,3 +13,13 @@ CREATE TABLE `tbp_sys_dao_columns` (
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
+
+
+INSERT INTO `tbm_sys_sql_magt` (`id`, `parent_id`, `type`, `name`, `content`) VALUES ('1', NULL, 'D', '학교', NULL);
+INSERT INTO `tbm_sys_sql_magt` (`id`, `parent_id`, `type`, `name`, `content`) VALUES ('2', '1', 'F', 'HTML5', 'ㅋㅋㅋ');
+INSERT INTO `tbm_sys_sql_magt` (`id`, `parent_id`, `type`, `name`, `content`) VALUES ('3', '1', 'F', '빅데이터', 'ㅇㅇ');
+INSERT INTO `tbm_sys_sql_magt` (`id`, `parent_id`, `type`, `name`, `content`) VALUES ('4', NULL, 'D', '회사', NULL);
+INSERT INTO `tbm_sys_sql_magt` (`id`, `parent_id`, `type`, `name`, `content`) VALUES ('5', NULL, 'D', '연구회', NULL);
+INSERT INTO `tbm_sys_sql_magt` (`id`, `parent_id`, `type`, `name`, `content`) VALUES ('6', '5', 'D', '동적비즈니스', NULL);
+INSERT INTO `tbm_sys_sql_magt` (`id`, `parent_id`, `type`, `name`, `content`) VALUES ('7', '6', 'D', '20160904', NULL);
+
