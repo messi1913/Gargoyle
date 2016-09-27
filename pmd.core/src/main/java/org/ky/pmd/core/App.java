85fed4ec98c9c21aa51e41e2e6bcdd526ccd5d89
Kim <callakrsos@naver.com>
2016-09-27 23:23:41 +0900

    pmd base

---
+++ pmd.core/src/main/java/org/ky/pmd/core/App.java
@@ -0,0 +1,13 @@
+package org.ky.pmd.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
