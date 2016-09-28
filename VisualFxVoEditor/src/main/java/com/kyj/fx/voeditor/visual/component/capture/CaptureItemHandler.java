aeead85dc36a1c445d3289b044a6c174c173a1f8
KYJ <callakrsos@naver.com>
2016-09-28 18:37:57 +0900

    fix

--- VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/capture/CaptureItemHandler.java
+++ VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/capture/CaptureItemHandler.java
@@ -11,11 +11,16 @@ import com.kyj.fx.voeditor.visual.util.ValueUtil;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeLineCap;
 import javafx.scene.transform.Scale;
 
 /**
@@ -48,7 +53,7 @@ public class CaptureItemHandler {
 			label.getTransforms().add(new Scale(5, 5));
 
 			label.setOnMouseClicked(ev -> {
-				
+
 				if (ev.getClickCount() == 2) {
 					DialogUtil.showInputDialog(btn, "Text", "Input Text").ifPresent(v -> {
 						//						v.getValue()
@@ -74,21 +79,79 @@ public class CaptureItemHandler {
 	 */
 	protected void createFigure() {
 
-		Button btn = new Button("사각형");
-		btn.setOnAction(e -> {
-			Rectangle rectangle = new Rectangle(200d, 200d);
-			rectangle.setStyle("-fx-background-color: red;");
-			//			rectangle.setFill(Color.RED);
-			rectangle.setFill(Color.TRANSPARENT);
-			rectangle.setStroke(Color.RED);
-			//			rectangle.getTransforms().add(new Scale(5, 2));
-			controller.addItemEvent(rectangle);
-			controller.addChildren(rectangle);
+		{
+			Button btn = new Button("사각형");
+			btn.setOnAction(e -> {
+				Rectangle rectangle = new Rectangle(200d, 200d);
+				rectangle.setStyle("-fx-background-color: red;");
+				//			rectangle.setFill(Color.RED);
+				rectangle.setFill(Color.TRANSPARENT);
+				rectangle.setStroke(Color.RED);
+				rectangle.setArcHeight(15);
+				rectangle.setArcWidth(15);
 
-			//			rectangle.getlay
-		});
+				//			rectangle.getTransforms().add(new Scale(5, 2));
+				controller.addItemEvent(rectangle);
+				controller.addChildren(rectangle);
 
-		items.add(btn);
+				//			rectangle.getlay
+			});
+
+			items.add(btn);
+		}
+
+		{
+			Button btn = new Button("라인");
+			btn.setOnAction(e -> {
+
+				//				Rectangle rectangle = new Rectangle(200d, 200d);
+				//				rectangle.setStrokeLineJoin(StrokeLineJoin.ROUND);
+				
+				
+				Line line = new Line();
+				line.setStartX(0d);
+				line.setStartY(0d);
+				line.setEndX(200d);
+				line.setEndY(200d);
+
+				line.setStrokeWidth(10);
+				line.setStrokeLineCap(StrokeLineCap.BUTT);
+				line.getStrokeDashArray().addAll(15d, 5d, 15d, 15d, 20d);
+				line.setStrokeDashOffset(10);
+
+				controller.addItemEvent(line);
+				controller.addChildren(line);
+			});
+			items.add(btn);
+		}
+
+		{
+			Button btn = new Button("둥근타원형");
+			btn.setOnAction(e -> {
+
+				//				Rectangle rectangle = new Rectangle(200d, 200d);
+				//				rectangle.setStrokeLineJoin(StrokeLineJoin.ROUND);
+				Group g = new Group();
+				DropShadow ds = new DropShadow();
+			    ds.setOffsetY(3.0);
+			    ds.setColor(Color.color(0.4, 0.4, 0.4));
+
+			    Ellipse ellipse = new Ellipse();
+			    ellipse.setCenterX(50.0f);
+			    ellipse.setCenterY(50.0f);
+			    ellipse.setRadiusX(50.0f);
+			    ellipse.setRadiusY(25.0f);
+			    ellipse.setFill(Color.RED);
+			    ellipse.setStroke(Color.BLACK);
+			    ellipse.setEffect(ds);
+			    g.getChildren().add(ellipse);
+
+				controller.addItemEvent(g);
+				controller.addChildren(g);
+			});
+			items.add(btn);
+		}
+
 	}
 
 	/**
