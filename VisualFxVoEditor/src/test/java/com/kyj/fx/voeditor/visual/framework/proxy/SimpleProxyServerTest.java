5b73a19831c76c68066dea2ff9fddd85d377af01
KYJ <callakrsos@naver.com>
2017-01-24 16:29:43 +0900

    proxy server test code

---
+++ VisualFxVoEditor/src/test/java/com/kyj/fx/voeditor/visual/framework/proxy/SimpleProxyServerTest.java
@@ -0,0 +1,59 @@
+/********************************
+ *	프로젝트 : VisualFxVoEditor
+ *	패키지   : com.kyj.fx.voeditor.visual.framework.proxy
+ *	작성일   : 2017. 1. 19.
+ *	작성자   : KYJ
+ *******************************/
+package com.kyj.fx.voeditor.visual.framework.proxy;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.kyj.fx.voeditor.visual.exceptions.GagoyleRuntimeException;
+
+/**
+ * @author KYJ
+ *
+ */
+public class SimpleProxyServerTest {
+	/**
+	 * @최초생성일 2017. 1. 19.
+	 */
+	private static final Charset FOR_NAME = Charset.forName("UTF-8");
+	SimpleProxyServer server;
+
+	@Before
+	public void setup() {
+		server = new SimpleProxyServer(8001, "localhost", 8000);
+	}
+
+	@Test
+	public final void test() throws GagoyleRuntimeException, IOException {
+		server.addOnRequestListener(new UTF8EncodingProxyListener() {
+
+			@Override
+			public void onAction(String str) {
+				System.out.printf("\n request : \n%s", str);
+			}
+
+		});
+
+		server.addOnResponseListener(new UTF8EncodingProxyListener() {
+
+			@Override
+			public void onAction(String str) {
+				System.out.printf("\n response : \n%s", str);
+			}
+
+		});
+
+		server.start();
+	}
+
+}
