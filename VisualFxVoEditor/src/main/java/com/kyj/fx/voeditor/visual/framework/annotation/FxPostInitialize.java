d38ffa933420ce8a4328e8792075cde60a5e8ec1
KYJ <callakrsos@naver.com>
2017-02-08 10:22:00 +0900

    코멘트추가.

--- VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/framework/annotation/FxPostInitialize.java
+++ VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/framework/annotation/FxPostInitialize.java
@@ -11,8 +11,9 @@ import java.lang.annotation.RetentionPolicy;
 
 /***************************
  *
- * Javafx initialize 처리후 수행될 후처리를 기술함.
+ * Javafx initialize 처리후 수행될 후처리를 기술함. <br>
  *
+ * <code>Platform.runLater() </code>함수로 호출되기때문에 지연호출된다는 점을 인식할것. <br>
  * @author KYJ
  *
  ***************************/
