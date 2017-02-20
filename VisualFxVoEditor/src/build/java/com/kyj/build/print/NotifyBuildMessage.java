8e0991640528f817d1645dbc6f08aff39761f682
KYJ <callakrsos@naver.com>
2017-02-20 16:44:54 +0900

    빌드 테스크 코드.

--- VisualFxVoEditor/src/build/java/com/kyj/build/print/NotifyBuildMessage.java
+++ VisualFxVoEditor/src/build/java/com/kyj/build/print/NotifyBuildMessage.java
@@ -6,19 +6,29 @@
  *******************************/
 package com.kyj.build.print;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
 import com.kyj.fx.voeditor.visual.momory.ResourceLoader;
 
 /**
+ *
+ * ant를 이용한 build.xml파일을 빌드할때
+ *
+ * 메세지 처리를 구현.
  * @author KYJ
  *
  */
 public class NotifyBuildMessage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {
 
+		URL resource = ClassLoader.getSystemClassLoader().getResource("");
+		File parentFile = new File(resource.toURI()).getParentFile().getParentFile().getParentFile();
+		System.out.println(parentFile.getAbsolutePath());
 		if (args == null || args.length == 0) {
 			System.out.println("Notify.");
 			return;
@@ -41,4 +51,31 @@ public class NotifyBuildMessage {
 
 		//		ValueUtil.toString(new RuntimeException("zz"));
 	}
+
+	//	private static final Supplier<Connection> supplier = new Supplier<Connection>() {
+	//
+	//		@Override
+	//		public Connection get() {
+	//			try {
+	//				String url = "jdbc:sqlite:" + ValueUtil.getBaseDir() + File.separator + "gargoyle.db";
+	//				return DbUtil.getConnection("org.sqlite.JDBC", url, new Properties());
+	//			} catch (ClassNotFoundException e) {
+	//				e.printStackTrace();
+	//			} catch (Exception e) {
+	//				// TODO Auto-generated catch block
+	//				e.printStackTrace();
+	//			}
+	//
+	//			return null;
+	//		}
+	//	};
+	//
+	//	void write() throws Exception {
+	//
+	//		try (Connection con = supplier.get()) {
+	//			List<Map<String, Object>> select = DbUtil.select(con, "select * from tbm_sys_envsd");
+	//			select.forEach(System.out::println);
+	//		}
+	//
+	//	}
 }
