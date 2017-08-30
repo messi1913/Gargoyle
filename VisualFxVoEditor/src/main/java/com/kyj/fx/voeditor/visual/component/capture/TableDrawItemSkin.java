47dcdadaca169f4cb00391f120ea96042314919d
callakrsos <callakrsos@naver.com>
2017-08-30 23:06:21 +0900

    upgrade erd table template

--- VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/capture/TableDrawItemSkin.java
+++ VisualFxVoEditor/src/main/java/com/kyj/fx/voeditor/visual/component/capture/TableDrawItemSkin.java
@@ -3,10 +3,15 @@
  */
 package com.kyj.fx.voeditor.visual.component.capture;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.kyj.fx.voeditor.visual.util.ValueUtil;
+
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 
 /**
@@ -23,13 +28,44 @@ public class TableDrawItemSkin extends DrawItemSkin<TableDrawItem> {
 	public void draw(VBox container) {
 		TableDrawItem control = getControl();
 
-		container.getChildren().add(new Label(control.getTitle()));
+		// container.getChildren().add(new Label(control.getTitle()));
 
-		List<Label> collect = control.getItems().stream().map(a -> new Label(a)).collect(Collectors.toList());
-		container.getChildren().addAll(collect);
+		// List<Label> collect = control.getItems().stream().map(a -> new Label(a)).collect(Collectors.toList());
+		// container.getChildren().addAll(collect);
 
-		// container.getChildren().add(new Label("Column-Name"));
-		// container.getChildren().add(new Label("Column-Name"));
+		try {
+			FXMLLoader loader = new FXMLLoader();
+
+			VBox vbox = loader.load(TableDrawItemSkin.class.getResourceAsStream("ErdBaseTemplate.fxml"));
+			vbox.getStylesheets().add(TableDrawItemSkin.class.getResource("ErdBaseTemplate.css").toExternalForm());
+
+			ErdBaseTemplateController controller = loader.getController();
+			ListView<String> lvColumns = controller.getLvColumns();
+
+			List<String> items = control.getItems();
+
+			if (items.isEmpty()) {
+				controller.setTableName("virtual-tableName");
+				lvColumns.getItems().add("");
+				lvColumns.getItems().add("");
+				lvColumns.getItems().add("");
+				lvColumns.getItems().add("");
+
+			} else {
+				String tableName = items.get(0);
+
+				controller.setTableName(tableName);
+				for (int i = 1; i < items.size(); i++) {
+					String col = items.get(i);
+					lvColumns.getItems().add(col);
+				}
+				container.getChildren().add(vbox);
+			}
+
+		} catch (IOException e) {
+			ValueUtil.toString(e);
+		}
+
 	}
 
 }
