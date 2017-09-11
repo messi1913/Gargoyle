e7820954995910f9aff5110c8df8c2fa781bb9e4
callakrsos <callakrsos@naver.com>
2017-09-11 13:37:04 +0900

    remove all query

--- VisualFxVoEditor/src/main/resources/META-INF/script/Mssql/sql.limit.sql
+++ VisualFxVoEditor/src/main/resources/META-INF/script/Mssql/sql.limit.sql
@@ -1,12 +1,8 @@
 /*2016.02.11 startRow, maxRow 변수 추가*/
-select a.* from ($usersql ) a
-where 1=1
-#if($startRow && $maxRow)
- and rownum between  $startRow and  $maxRow
-#elseif($startRow)
- and rownum <= $startRow
-#elseif($maxRow)
- and rownum <= $maxRow
+select 
+#if($maxRow)
+top $maxRow
 #else
-and rownum <= 100
+top 100
 #end
+a.* from ($usersql ) a
